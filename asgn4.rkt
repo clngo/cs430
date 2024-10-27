2f97d7f4dbe20a12f399e191c139e0d4f35f6ea7
Colin Ngo <113945637+clngo@users.noreply.github.com>
2024-10-27 15:14:29 -0700

    Update asgn4.rkt

--- asgn4.rkt
+++ asgn4.rkt
@@ -1,5 +1,7 @@
 #lang typed/racket
 
+; Passed all test cases! Fully completed :D
+
 (require typed/rackunit)
 	
 (define-type ExprC (U NumC IdC StrC IfC AppC LamC))
@@ -45,20 +47,43 @@
     [(list 'if x then else) (IfC (parse x) (parse then) (parse else))]
 
     ; Bind
-    [(list 'bind clauses ... body) (StrC "testing")]
+    [(list 'bind clauses ... body)
+     (define symbols (extract-symbols (cast clauses Sexp) '()))
+     (define exprs (extract-expressions (cast clauses Sexp) '()))
+     (if (equal? (length symbols) (length (remove-duplicates symbols)))
+         (AppC (LamC symbols (parse body)) exprs)
+         (error 'parse "AAQZ : Duplicate Arguments in Function ~a" symbols))]
+
     
     ; LamC
     [(list (list (? symbol? param) ...) '=> body)
-     (cond [(check-duplicates param) (error 'parse "AAQZ : Duplicate Arguments in Function")]
+     (cond [(check-duplicates param) (error 'parse "AAQZ : Duplicate Arguments in Function ~a" param)]
            [else (LamC (valid-args (cast param (Listof Symbol))) (parse body))])]
 
     ; AppC
     [(list fun param ...) (AppC (parse fun) (map parse param))]
 
     ; IdC
-    [(? symbol? sym) (IdC sym)]
+    [(? symbol? sym) (valid-id? sym) (IdC sym)]
+    
     [other (error 'parse "AAQZ : Invalid S-Expression ~e" other)]))
 
+;; Helper for desugaring that extracts symbols for LamC's parameters
+(define (extract-symbols [binding-clauses : Sexp] [accumulated-ids : (Listof Symbol)]) : (Listof Symbol)
+  (match binding-clauses
+    [(list (list (? symbol? ids) '= expr))
+     (valid-id? ids) (extract-symbols '() (cons ids accumulated-ids))]
+    [(list (list (? symbol? ids) '= expr) binding-clauses ...)
+     (extract-symbols binding-clauses (cons ids accumulated-ids))]
+    ['() (reverse accumulated-ids)]
+    [other (error 'extract-symbols "AAQZ : Invalid binding clause structure: ~a" binding-clauses)]))
+
+(define (extract-expressions [clauses : Sexp] [accumulated-exprs : (Listof ExprC)]) : (Listof ExprC)
+  (match clauses
+    [(list (list (? symbol? id) '= exp)) (extract-expressions '() (cons (parse exp) accumulated-exprs))]
+    [(list (list (? symbol? id) '= exp) clauses ...) (extract-expressions clauses (cons (parse exp) accumulated-exprs))]
+    ['() (reverse accumulated-exprs)]
+    [other (error 'extract-expressions "AAQZ : Invalid binding clause structure: ~a" clauses)]))
 
 ; Checks if the given symbol is a valid identifier
 (define (valid-id? [sym : Any]) : Boolean
@@ -91,10 +116,11 @@
     [(StrC str) (StrV str)]
     [(IdC sym) (lookup sym env)]
     [(IfC x t e) (define boo (interp x env))
-                       (cond [BoolV? boo
-                                     (cond [(equal? boo (BoolV #t)) (interp t env)]
+                 (match boo
+                   [(BoolV bool) (cond [(equal? bool #t) (interp t env)]
                                            [else (interp e env)])]
-                             [else (error 'interp "AAQZ : Given non-boolean type")])]
+                   [other (error 'interp "AAQZ : Given non-boolean type")])]
+                             
     [(LamC p b) (CloV p b env)]
     [(AppC f a) (define f-value (interp f env))
                 (define val-lst (map (lambda ([arg : ExprC]) (interp arg env)) a))
@@ -110,7 +136,7 @@
                                         ['error (error 'interp "AAQZ : user-error ~s" (serialize (first val-lst)))]
                                         [other (error 'interp "AAQZ : Wrong number of args")])]
                                      [else (error 'interp "AAQZ : Wrong number of args")])]
-                  [other (error 'interp "AAQZ : AppC is wrong")])]))
+                  [other (error 'interp "AAQZ : closure or primitive in function application")])]))
 
 ; Finds binding in env based given a symbol
 (define (lookup [for : Symbol] [env : Env]) : Value
@@ -132,17 +158,17 @@
 (define (interp-PrimV [sym : Symbol] [l : Value] [r : Value]) : Value
   (match sym
     ['+ (cond [(and (NumV? l) (NumV? r)) (NumV (+ (NumV-num l) (NumV-num r)))]
-              [else (error 'interp-PrimV "one argument was not a number")])]
+              [else (error 'interp-PrimV "AAQZ : one argument was not a number")])]
     ['- (cond [(and (NumV? l) (NumV? r)) (NumV (- (NumV-num l) (NumV-num r)))]
-              [else (error 'interp-PrimV "one argument was not a number")])]
+              [else (error 'interp-PrimV "AAQZ : one argument was not a number")])]
     ['* (cond [(and (NumV? l) (NumV? r)) (NumV (* (NumV-num l) (NumV-num r)))]
-              [else (error 'interp-PrimV "one argument was not a number")])]
+              [else (error 'interp-PrimV "AAQZ : one argument was not a number")])]
     ['/ (cond [(and (NumV? l) (NumV? r))
                (cond [(zero? (NumV-num r)) (error 'interp-PrimV "AAQZ : Cannot divide by 0")]
                      [else (NumV (/ (NumV-num l) (NumV-num r)))])]
-              [else (error 'interp-PrimV "one argument was not a number")])]
+              [else (error 'interp-PrimV "AAQZ : one argument was not a number")])]
     ['<= (cond [(and (NumV? l) (NumV? r)) (BoolV (<= (NumV-num l) (NumV-num r)))]
-               [else (error 'interp-PrimV "one argument was not a number")])]
+               [else (error 'interp-PrimV "AAQZ : one argument was not a number")])]
     ; Functions like eq?
     ; Neither l or r is a CloV or PrimV and 2 values are equal
     ['equal? (cond [(and (and (not (CloV? l)) (not (PrimV? l))) (and (not (CloV? r)) (not (PrimV? r))))
@@ -159,6 +185,7 @@
 ; ----------------------------------- TEST CASES -----------------------------------
 ; ----------------------------------------------------------------------------------
 
+; ----------------------------------- TEST PARSING FUNCTIONS -----------------------------------
 ; parse
 (check-equal? (parse '1) (NumC 1))
 (check-equal? (parse 'x) (IdC 'x))
@@ -172,15 +199,56 @@
 (check-equal? (parse '(g 3 4)) (AppC (IdC 'g) (list (NumC 3) (NumC 4))))
 (check-equal? (parse '(+ 1 2)) (AppC (IdC '+) (list (NumC 1) (NumC 2))))
 (check-equal? (parse '(<= 1 2)) (AppC (IdC '<=) (list (NumC 1) (NumC 2))))
-(check-equal? (parse '(+ 1)) (AppC (IdC '+) (list (NumC 1)))) ; valid in racket, invalid AAQZ4???
+(check-equal? (parse '(+ 1)) (AppC (IdC '+) (list (NumC 1)))) ; valid in racket and AAQZ4; pass parse, fails interp
 (check-equal? (parse '({x} => 1)) (LamC (list 'x) (NumC 1)))
 (check-equal? (parse '({x} => x)) (LamC (list 'x) (IdC 'x)))
+(check-exn (regexp (regexp-quote "AAQZ : Duplicate Arguments in Function (x x)"))
+           (lambda () (parse '({x x} => (+ x 1)))))
+#; (check-equal? (parse '({x y} => x)) (LamC (list 'x 'y) (IdC 'x)))
 (check-equal? (parse '{{x} => +}) (LamC (list 'x) (IdC '+)))
 (check-equal? (parse '{{} => 0}) (LamC '() (NumC 0)))
-#; (check-equal? (parse '{{a} => {+ a}}) (LamC (list (IdC 'a)) (NumC 5))) ; ?????????????????
-(check-equal? (parse '{{+} => 0}) (LamC (list '+) (NumC 0))) ; valid in racket, invalid AAQZ4???
+(check-equal? (parse '{{+} => 0}) (LamC (list '+) (NumC 0))) ; valid in racket and AAQZ4???
+(check-equal? (parse '(bind [x = 1] {+ 1 x}))
+              (AppC
+               (LamC (list 'x) (AppC (IdC '+) (list (NumC 1) (IdC 'x))))
+               (list (NumC 1))))
+(check-equal? (parse '(bind [x = 1] [y = 2] {+ x y}))
+              (AppC
+               (LamC (list 'x 'y) (AppC (IdC '+) (list (IdC 'x) (IdC 'y))))
+               (list (NumC 1) (NumC 2))))
 
-  
+(check-exn (regexp (regexp-quote "AAQZ : Invalid Id 'bind"))
+           (lambda () (parse '(bind [bind = 1] bind))))
+(check-exn (regexp (regexp-quote "AAQZ : Duplicate Arguments in Function (x x)"))
+           (lambda () (parse '(bind [x = 1] [x = 2] {+ 1 x}))))
+(check-exn (regexp (regexp-quote "AAQZ : Invalid S-Expression #t"))
+           (lambda () (parse #t)))
+
+
+;extract symbols
+(check-equal? (extract-symbols '() '()) '())
+(check-equal? 
+  (extract-symbols '((x = (+ 1 2)) (y = (* 3 4))) '()) 
+  '(x y))
+(check-equal? (extract-symbols '((x = 2) (y = 3)) '()) 
+              '(x y))
+(check-equal? (extract-symbols '((x = 42)) '()) 
+              '(x))
+(check-exn (regexp (regexp-quote "AAQZ : Invalid Id 'bind"))
+           (lambda () (extract-symbols '((bind = 42)) '())))
+(check-exn (regexp (regexp-quote "AAQZ : Invalid binding clause structure: (bind this is wrong not valid)"))
+           (lambda () (extract-symbols '(bind this is wrong not valid) '())))
+
+; extract-expressions
+(check-equal? (extract-expressions '() '()) '())
+(check-equal? 
+  (extract-expressions '((x = (+ 1 2)) (y = (* 3 4))) '()) 
+  (list (AppC (IdC '+) (list (NumC 1) (NumC 2))) (AppC (IdC '*) (list (NumC 3) (NumC 4)))))
+(check-equal? (extract-expressions '((x = 2) (y = 3)) '()) 
+              (list (NumC 2) (NumC 3)))
+(check-exn (regexp (regexp-quote "AAQZ : Invalid binding clause structure: (bind this is wrong not valid)"))
+           (lambda () (extract-expressions '(bind this is wrong not valid) '())))
+
 ; valid-id?
 (check-equal? (valid-id? 'test) #t)
 (check-equal? (valid-id? '+) #t)
@@ -191,6 +259,8 @@
 
 ; valid-args
 (check-equal? (valid-args '(a b c d e)) (list 'a 'b 'c 'd 'e))
+
+; ----------------------------------- TEST INTERP FUNCTIONS -----------------------------------
 
 ; interp
 (check-equal? (interp (NumC 1) top-env) (NumV 1))
@@ -213,6 +283,8 @@
 (check-equal? (interp (IfC (AppC (IdC 'equal?) (list (AppC (IdC '+) (list (NumC 1) (NumC 2))) (NumC 2)))
                            (IdC 'true) (StrC "False PrimV")) top-env)
                       (StrV "False PrimV"))
+(check-exn (regexp (regexp-quote "AAQZ : Given non-boolean type"))
+           (lambda () (interp (IfC (StrC "string") (NumC 3) (NumC 0)) top-env)))
 (check-equal? (interp (LamC (list 'x) (NumC 1)) top-env) (CloV (list 'x) (NumC 1) top-env))
 (check-equal? (interp (LamC '() (StrC "Call with no args")) top-env)
               (CloV '() (StrC "Call with no args") top-env))
@@ -232,7 +304,10 @@
 (check-equal? (interp (AppC (LamC (list 'x)
                                   (AppC (IdC '+) (list (IdC 'x) (NumC 1))))
                             (list (NumC 7))) top-env)
-              (NumV 8)) 
+              (NumV 8))
+(check-exn (regexp (regexp-quote "AAQZ : Wrong number of args"))
+           (lambda () (interp (AppC (LamC (list 'x) (NumC 1)) (list (NumC 1) (NumC 2))) top-env)))
+
 #; (check-equal? (interp (AppC (LamC (list 'f 'g)
                                   (LamC (list 'x)
                                         (AppC (IdC 'f)
@@ -250,8 +325,6 @@
 (check-exn (regexp (regexp-quote "AAQZ : Wrong number of args"))
            (lambda () (interp (AppC (IdC 'error) (list (NumC 1) (NumC 2) (NumC 3))) top-env)))
 
-
-
 ; interp-PrimV
 (check-equal? (interp-PrimV '+ (NumV 1) (NumV 0)) (NumV 1))
 (check-equal? (interp-PrimV '- (NumV 5) (NumV 3)) (NumV 2))
@@ -267,7 +340,6 @@
 (check-equal? (interp-PrimV 'equal? (StrV "test1") (StrV "test1")) (BoolV #t))
 (check-equal? (interp-PrimV 'equal? (PrimV '+) (StrV "test1")) (BoolV #f))
 (check-equal? (interp-PrimV 'equal? (CloV '() (NumC 1) top-env) (NumV 1)) (BoolV #f))
-
 
 
 ; lookup
@@ -305,8 +377,37 @@
 (check-equal? (top-interp '(((x y) => (if (equal? x y) "match" "no match")) 3 4)) "\"no match\"")
 (check-equal? (top-interp '(((x) => (* x x)) 4)) "16")
 (check-equal? (top-interp '(((x) => (((y) => (+ x y)) 10)) 5)) "15")
-#; (check-equal? (top-interp '(((n) => {if {<= n 0} 1 {* n {fact {- n 1}}}}) 4)) "error")
+(check-exn (regexp (regexp-quote "AAQZ : name not found: 'fact"))
+           (lambda () (top-interp '(bind [fact = ((n) => {if {<= n 0} 1 {* n {fact {- n 1}}}})] (fact 4)))))
+(check-equal? (top-interp '(bind [fact = ((self n) => {if {<= n 0} 1 {* n {self self {- n 1}}}})] (fact fact 4))) "24")
 (check-equal? (top-interp '(((f g) => (((x) => (g (f x))) 2))
                             ((x) => (+ x 3))
                             ((y) => (* y 2))))
               "10")
+
+(check-exn (regexp (regexp-quote "AAQZ : user-error \"1\""))
+           (lambda () (top-interp '(((x) => (error x)) 1))))
+(check-exn (regexp (regexp-quote "AAQZ : user-error \"8\""))
+           (lambda () (top-interp '(((x) => (error x)) (((y) => (+ y 3)) 5)))))
+(check-exn (regexp (regexp-quote "AAQZ : user-error \"true\""))
+           (lambda () (top-interp '(((x) => (error x))
+                                    (((y) => (equal? y 5)) 5)))))
+(check-exn (regexp (regexp-quote "AAQZ : user-error \"false\""))
+           (lambda () (top-interp '(((x) => (error x))
+                                    (((y) => (equal? y 5)) 4)))))
+(check-exn (regexp (regexp-quote "AAQZ : closure or primitive in function application"))
+           (lambda () (top-interp '(1 2 3 4))))
+(check-exn (regexp (regexp-quote "AAQZ : one argument was not a number"))
+           (lambda () (top-interp '(((x y) => (+ x y)) 1 "1"))))
+(check-exn (regexp (regexp-quote "AAQZ : one argument was not a number"))
+           (lambda () (top-interp '(((x y) => (- x y)) 1 "1"))))
+(check-exn (regexp (regexp-quote "AAQZ : one argument was not a number"))
+           (lambda () (top-interp '(((x y) => (* x y)) 1 "1"))))
+(check-exn (regexp (regexp-quote "AAQZ : one argument was not a number"))
+           (lambda () (top-interp '(((x y) => (/ x y)) 1 "1"))))
+(check-exn (regexp (regexp-quote "AAQZ : Cannot divide by 0"))
+           (lambda () (top-interp '(((x y) => (/ x y)) 1 0))))
+(check-exn (regexp (regexp-quote "AAQZ : one argument was not a number"))
+           (lambda () (top-interp '(((x y) => (<= x y)) 1 "1"))))
+
+
